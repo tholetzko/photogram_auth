65a0bb8c581a141c5bc0407a4d845b4357cbdc79
Tobias <tholetzko@users.noreply.github.com>
2017-05-30 18:54:32 -0500

    Updated photo page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -25,7 +25,7 @@ class LikesController < ApplicationController
     save_status = @like.save
 
     if save_status == true
-      redirect_to("/likes/#{@like.id}", :notice => "Like created successfully.")
+      redirect_to("/photos", :notice => "Like created successfully.")
     else
       render("likes/new.html.erb")
     end
@@ -54,11 +54,9 @@ class LikesController < ApplicationController
 
   def destroy
     @like = Like.find(params[:id])
-
     @like.destroy
-
-    if URI(request.referer).path == "/likes/#{@like.id}"
-      redirect_to("/", :notice => "Like deleted.")
+    if URI(request.referer).path == "/photos"
+      redirect_to("/photos", :notice => "Like deleted.")
     else
       redirect_to(:back, :notice => "Like deleted.")
     end
